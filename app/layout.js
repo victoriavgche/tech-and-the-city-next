a1e19a4df4568fd77a74d1827d4c15adeefcf098
victoriavgche <-EMAIL-@.com>
2025-09-23 19:29:28 +0300

    Add Google Fonts link for Space Grotesk in HTML head

--- app/layout.js
+++ app/layout.js
@@ -67,6 +67,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
